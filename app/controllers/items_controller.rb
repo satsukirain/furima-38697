89ea82b2597f1676a5b164ad26658cb8f6f24ff6
satsukirain <117341700+satsukirain@users.noreply.github.com>
2022-12-20 21:56:52 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
+    before_action :set_item, only: [ :show, :edit, :update]
+
     def index
       @items = Item.all.order("created_at DESC")
     end
@@ -18,16 +20,29 @@ class ItemsController < ApplicationController
     end
 
     def edit
-    #   @item = Item.all
+      if @item.user != current_user  
+        redirect_to root_path
+      end
     end
 
     def show
-      @item = Item.find(params[:id])
     end
 
+    def update
+      if @item.update(item_params)
+        redirect_to item_path
+      else
+        render :edit
+      end
+    end
+    
     private
 
     def item_params
         params.require(:item).permit(:product_name, :explanation, :category_id, :situation_id, :shipping_charge_id, :region_of_origin_id, :number_of_day_id, :price, :image).merge(user_id: current_user.id)
+    end
+  
+    def set_item
+      @item = Item.find(params[:id])
     end
 end
